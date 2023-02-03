805b8b3358edb01f5cecb9fde5d195a735134070
djdiptayan1 <111650788+djdiptayan1@users.noreply.github.com>
2023-02-03 22:28:45 +0530

    updated codes

---
+++ attendance/attendance.ino
@@ -0,0 +1,60 @@
+/* Read RFID Tag with RC522 RFID Reader
+ *  Made by miliohm.com
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+constexpr uint8_t RST_PIN = D3;  // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = D4;   // Configurable, see typical pin layout above
+
+MFRC522 rfid(SS_PIN, RST_PIN);  // Instance of the class
+MFRC522::MIFARE_Key key;
+
+String tag;
+
+void setup() {
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+  Serial.begin(9600);
+  SPI.begin();      // Init SPI bus
+  rfid.PCD_Init();  // Init MFRC522
+}
+
+void loop() {
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("scan");
+  lcd.setCursor(1, 1);
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
+  if (rfid.PICC_ReadCardSerial()) {
+    for (byte i = 0; i < 4; i++) {
+      tag += rfid.uid.uidByte[i];
+    }
+    Serial.println(tag);
+    if (tag == "249224147101") {
+        lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Welcome");
+  lcd.setCursor(1, 1);
+      Serial.println("Diptayan");
+      lcd.println("Diptayan");
+      delay(2000);
+    } else {
+        lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("SUS!!!");
+  lcd.setCursor(1, 1);
+      lcd.print(tag);
+      Serial.println(tag);
+      delay(2000);
+    }
+    tag = "";
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+  }
+}
