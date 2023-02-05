022919a9fe64d19c92a4f71f9cd77f818dba5443
djdiptayan1 <111650788+djdiptayan1@users.noreply.github.com>
2023-02-05 22:31:43 +0530

    update

--- writing_and_reading_file/writing_and_reading_file.ino
+++ writing_and_reading_file/writing_and_reading_file.ino
@@ -3,6 +3,8 @@
 
 File myfile; //create a file named attendance
 
+constexpr uint8_t cs = D8;//NODE MCU
+
 void setup(){
   Serial.begin(9600);
   while(!Serial){
@@ -11,7 +13,8 @@ void setup(){
 
   Serial.println("Initializing SD card");
 
-  if(!SD.begin(4)){
+ // if(!SD.begin(4)){ //arduino
+    if(!SD.begin(cs)){ //nodeMCU
     Serial.println("initialization failed");
     while(1);
   }
@@ -28,13 +31,13 @@ void setup(){
   Serial.println("WROTE IN FILE SUCCESSFUL");
   myfile.close();
 
-// //reading a file
-//   myfile=SD.open("attendance.txt", FILE_READ);
-//   myfile.write("READING :) :)");
-//   while(myfile.available()){
-//     Serail.write(myfile.read());
-//   }
-//   myfile.close();
+//reading a file
+  myfile=SD.open("attendance.txt", FILE_READ);
+  myfile.write("READING :) :)");
+  while(myfile.available()){
+    Serial.write(myfile.read());
+  }
+  myfile.close();
 }
 void loop(){
   
